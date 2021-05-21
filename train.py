e8822df582d4a38df14989f2a288850c030e4633
cestwc <80936226+cestwc@users.noreply.github.com>
2021-05-21 23:41:10 +0800

    Update train.py

--- train.py
+++ train.py
@@ -117,7 +117,7 @@ def train(d, g, iterator, optD, optG, lossD, lossG, valid_iterator, N_EPOCHS = 3
 					best_valid_loss = valid_loss
 					torch.save(d.state_dict(), dirD)
 					torch.save(g.state_dict(), dirG)
-					print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
+				print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
 				d.train()				
 
 			# Save Losses for plotting later
