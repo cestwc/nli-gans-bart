901dadd030f4721c300e24b2dad84b6ae53b7a96
cestwc <80936226+cestwc@users.noreply.github.com>
2021-05-21 22:52:12 +0800

    Update util_train.py

--- util_train.py
+++ util_train.py
@@ -75,3 +75,9 @@ def categorical_accuracy(preds, y):
     correct = top_pred.eq(y.view_as(top_pred)).sum()
     acc = correct.float() / y.shape[0]
     return acc
+
+def epoch_time(start_time, end_time):
+	elapsed_time = end_time - start_time
+	elapsed_mins = int(elapsed_time / 60)
+	elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+	return elapsed_mins, elapsed_secs
